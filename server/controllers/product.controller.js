3d23a83d8d3496c3037e5b79e8b5f6d15d707fd6
Ashwin <2310030280@klh.edu.in>
2026-03-10 23:02:50 +0530

    added product logic

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,86 @@
+import product from '../models/product.model.js';
+
+export const getProducts = async(req,res,next)=>{
+    try{
+        const products = await product.find();
+        res.status(200).json({
+            success: true,
+            count: products.length,
+            data: products,
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const getProductById = async(req,res,next)=>{
+    try{
+        const product = await Product.findById(req.params.id);
+        if(!product){
+            const error = new Error('Product not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({
+            success: true,
+            data: product,
+        });
+    }catch(error){
+        next(error);
+    }
+}
+
+export const createProduct = async(req,res,next)=>{
+    try{
+        const product = await Product.create({
+            ...req.body,
+            createdBy:req.user._id,
+        });
+        res.status(201).json({
+            success: true,
+            message: 'Product created successfully',
+            data: product,
+        });
+    }catch(error){
+        next(error);
+    }
+}
+export const updateProduct = async(req,res,next)=>{
+    try{
+        const product = await Product.findByIdAndUpdate(
+            req.params.id,
+             req.body,
+             { new: true, runValidators: true }
+        );
+        if(!product){
+            const error = new Error('Product not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({
+            success: true,
+            message: 'Product updated successfully',
+            data: product,
+        });
+    }catch(error){
+        next(error);
+    }
+}
+
+export const deleteProduct = async(req,res,next)=>{
+    try{
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if(!product){
+            const error = new Error('Product not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({
+            success: true,
+            message: 'Product deleted successfully',
+            data: product,
+        });
+    }catch(error){
+        next(error);
+    }
+}
